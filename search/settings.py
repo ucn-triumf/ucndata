575fb9032613f3d56e4861f625ff3d2128b5a1e5
Derek Fujimoto <dfujimoto@triumf.ca>
2024-11-19 14:16:10 -0800

    Removed search

--- search/settings.py
+++
@@ -1,9 +0,0 @@
-# general settings for the database read/write operations
-# Derek Fujimto
-# Nov 2024
-
-# filename of data table
-filename = 'dataset.parquet'
-
-# columns to display after search
-output_columns = ['year', 'experiment', 'title', 'duration_hours', 'beam_current_mean']
