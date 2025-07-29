f48cdc0d0642b7aada4834e6c844d59e2fbc6004
Derek Fujimoto <dfujimoto@triumf.ca>
2025-07-29 09:23:26 -0700

    Saturation analysis

--- TCN6A_040/TCN6A_040.py
+++ TCN6A_040/TCN6A_040.py
@@ -11,216 +11,239 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 # settings
-ucnrun.datadir = '/mnt/ucndata/' # comment out if on daq04
 ucnrun.cycle_times_mode = 'li6'   # what frontend to use for determining cycle times [li6|he3|matched|sequencer|beamon]
-dirname = 'TCN6A_040'
-filename_summary = f'{dirname}/summary.csv'
+dirname = '.'
+outfile = f'{dirname}/summary.csv'
 
 target_currents = np.array([0.3, 1, 5, 10, 20, 36])
 
-# runs
-run_numbers = [ # 2549, # count rate issues
-                # 2550, # low rate
-                2551,
-                2552,
-                2553,
-                2554,
-                2555,
-                2556,
-                2557,
-                2575,
-                2576,
-                2577,
-                2579,
-                2580,
-                2581,
-                2582,
-                2584,
-                2585,
-                2587,
-                2588,
-                2590,
-                2592]
-
 os.makedirs(dirname, exist_ok=True)
 
-def cycle_filter(run):
-    """Determine if a cycle should be kept or not"""
-    filt = []
-    iterator = tqdm(run,
-                    desc=f'Run {run.run_number}: Scanning cycles',
-                    leave=False,
-                    total=run.cycle_param.ncycles)
+# worker fn
+def fetch(run, cyclen, last20):
+    current = run[cyclen, 0].beam1u_current_uA
+    current = current[current > 0]
 
-    for cyc in iterator:
+    output = {}
+    output['current'] = current.mean()
+    output['counts'] = run[cyclen, 2].get_nhits('Li6')
+    output['counts_bkgd'] = run[cyclen, 3].get_nhits('Li6')
+    output['run'] = run.run_number
+    output['cycle_num'] = cyclen
+    output['irradiation_time'] = run.cycle_param.period_durations_s.loc[0, cyclen]
+    output['storage_time'] = run.cycle_param.period_durations_s.loc[1, cyclen]
+    output['count_time'] = run.cycle_param.period_durations_s.loc[2, cyclen]
+    output['bkgd_time'] = run.cycle_param.period_durations_s.loc[3, cyclen]
+    output['is_last_20s'] = last20
 
-        # check if period duration exceeds cycle duration
-        expected_duration = cyc.cycle_param.period_durations_s.sum()
-        actual_duration = cyc.cycle_stop - cyc.cycle_start
-        if expected_duration > actual_duration:
-            filt.append(False)
-            tqdm.write(f'Run {run.run_number} (cycle {cyc.cycle}): cycle duration shorter than sum of periods')
-            continue
+    return pd.DataFrame(output, index=[0])
 
-        # drop cycles where the 1A beam drops to zero during production
-        if any(cyc[0].beam1a_current_uA < 0.1):
-            filt.append(False)
-            tqdm.write(f'Run {run.run_number} (cycle {cyc.cycle}): 1A current dropped below 0.1 uA during irradiation')
-            continue
+def fix_bad_transition(run, cycle, dt, kill_bkgd):
+    """Shift bad cycle timings by a constant amount"""
 
-        # check that there are hits in the count period
-        hits = cyc[2].get_hits('Li6')
-        hitsb = cyc[3].get_hits('Li6')
-        if hits.empty and hitsb.empty:
-            filt.append(False)
-            tqdm.write(f'Run {run.run_number} (cycle {cyc.cycle}): No counts in both counting period and background period')
-            continue
+    if kill_bkgd:
+        run.modify_timing(cycle, 3, 0, -120)
+        run.modify_timing(cycle, 0, 0, dt)
+        run.modify_timing(cycle, 1, 0, dt)
+        run.modify_timing(cycle, 2, 0, 120)
+    else:
+        run.modify_timing(cycle, 0, 0, dt)
+        run.modify_timing(cycle, 1, 0, dt)
+        run.modify_timing(cycle, 2, 0, dt)
 
-        # use background if counting period is empty
-        if hits.empty:
-            count_cyc = cyc[3]
-        else:
-            count_cyc = cyc[2]
+# get run data - 6A no foil
+def extract():
+    """*/120/120"""
 
-        # check for bad cycle definitions: rate close to irradiation rate at start of count
-        # note that irr counts comes in bunches for some reason
-        _, irr_hits = cyc[0].get_hits_histogram('Li6', bin_ms=100)
-        _, cnt_hits = count_cyc.get_hits_histogram('Li6', bin_ms=100)
-        irr_rate = irr_hits.max()/0.1
-        cnt_rate = cnt_hits.max()/0.1
+    runs = [# 2549, # count rate issues
+            # 2550, # low rate
+            2546, 2547, 2548, 2551, 2552, 2553, 2554, 2555, 2556, 2557, 2575,
+            2576, 2577, 2579, 2580, 2581, 2582, 2584, 2585, 2587, 2588, 2590,
+            2592]
 
-        if cnt_rate/irr_rate > 0.7:
-            filt.append(False)
-            tqdm.write(f'Run {run.run_number} (cycle {cyc.cycle}): Starting count rate ({int(cnt_rate):d} Hz) more than 70% of irradiation rate ({int(irr_rate):d} Hz)')
-            continue
+    df_list = []
 
-        filt.append(True)
-    return filt
+    for runn in tqdm(runs, desc="Runs"):
 
-def inspect_all():
-    for r in run_numbers:
-        run = ucnrun(r)
-        run.set_cycle_filter(cycle_filter(run))
-        run.inspect_beam()
-        plt.pause(1)
-        input()
-        plt.close()
+        run = ucnrun(runn)
+        run.set_cycle_filter(run.gen_cycle_filter())
 
-# extract counts from runs
-def extract_counts():
+        if runn == 2546:
 
-    # setup output file
-    header = ['# TCN6A-040 runs processed for backgrounds and counts',
-              '# Derek Fujimoto',
-             f'# {datetime.now()}']
+            # fix bad cycles
+            fix_bad_transition(run, 12, 20, False)
 
-    with open(filename_summary, 'w') as fid:
-        fid.write('\n'.join(header))
-        fid.write('\n')
+            # fixes to filter
+            cyc_filter = run.cycle_param.filter
+            cyc_filter[4] = False
+            cyc_filter[7] = False
+            cyc_filter[11] = False
+            cyc_filter[16] = False
+            run.set_cycle_filter(cyc_filter)
 
-    # extract
-    write_cols_names = True
-    for n in tqdm(run_numbers, desc='Parsing runs', leave=False):
+            # fix overal timing issues
+            for cyc in run:
+                cyc[1].modify_timing(1,1)
+                cyc[2].modify_timing(0,1)
 
-        # read
-        run = ucnrun(n)
-        run.to_dataframe()
+        elif runn == 2547:
 
-        # apply cycle filter
-        run.set_cycle_filter(cycle_filter(run))
+            # fix bad cycles
+            fix_bad_transition(run, 4, 40, False)
 
-        # get IV001 status - determine if background cycle
-        is_background = [cyc[2].tfile.UCN2EpPha5Tmp.UCN2_ISO_IV001_STATON.mean() for cyc in run]
-        is_background = ~(np.array(is_background)>0)
+        elif runn == 2548:
 
-        # get count cycle
-        counts = run[:,2].get_counts('Li6')
+            # fix bad cycles
+            fix_bad_transition(run, 4, -90, False)
+            fix_bad_transition(run, 9, 50, True)
+            fix_bad_transition(run, 14, -40, True)
+            fix_bad_transition(run, 22, 20, False)
 
-        # get cycles that are not background and background
-        bkgd_cyc = [cyc for is_bkgd, cyc in zip(is_background, run) if is_bkgd]
-        cnt_cyc = [cyc for is_bkgd, cyc in zip(is_background, run) if not is_bkgd]
+            # fixes to filter
+            cyc_filter = run.cycle_param.filter
+            cyc_filter[4] = True
+            cyc_filter[13] = False
+            cyc_filter[14] = True
+            cyc_filter[21] = False
+            cyc_filter[25] = False
+            run.set_cycle_filter(cyc_filter)
 
-        # skip if no background and count
-        if not bkgd_cyc or not cnt_cyc:
-            tqdm.write(f'Skipping run {n}, no background or count cycle present')
-            continue
+        elif runn in (2551, 2552, 2553, 2554, 2555, 2556, 2557, 2577, 2580,
+                      2581, 2582, 2584, 2585, 2587, 2590):
+            pass
 
-        # get average hits during backgrounds - weighted mean
-        counts, err = np.array([bk[3].get_counts('Li6') for bk in bkgd_cyc]).transpose()
-        bk_mean = np.average(counts, weights=1/err**2)
-        bk_std = 1/np.sum(1/err**2)**0.5
+        elif runn == 2575:
 
-        # get average beam current during background cycles
-        bk_currents = [bk[0].beam_current_uA for bk in bkgd_cyc]
-        bk_currents = pd.concat(bk_currents)
+            # fix bad cycles
+            fix_bad_transition(run, 5, -90, False)
+            fix_bad_transition(run, 8, 50, False)
+
+            # fixes to filter
+            cyc_filter = run.cycle_param.filter
+            cyc_filter[0] = True
+            run.set_cycle_filter(cyc_filter)
+
+        elif runn == 2576:
+            for cyc in run:
+                cyc[1].modify_timing(1,1)
+                if cyc.cycle in (1, 4):
+                    cyc[3].modify_timing(0,1)
+                else:
+                    cyc[2].modify_timing(0,1)
+
+        elif runn == 2579:
+
+            # fixes to filter
+            cyc_filter = run.cycle_param.filter
+            cyc_filter[0] = True
+            run.set_cycle_filter(cyc_filter)
+
+        elif runn == 2588:
+
+            # fixes to filter
+            cyc_filter = run.cycle_param.filter
+            cyc_filter[0] = True
+            run.set_cycle_filter(cyc_filter)
+
+        # extract counts
+        for cyc in run:
+            df_list.append(fetch(run, cyc.cycle, False))
+
+        # extract last 20s
+        for cyc in run:
+            if cyc.cycle_param.period_durations_s[2] > 0:
+                run.modify_timing(cyc.cycle, 2, 100, 0)
+            elif cyc.cycle_param.period_durations_s[3] > 0:
+                run.modify_timing(cyc.cycle, 3, 100, 0)
+
+        for cyc in run:
+            df_list.append(fetch(run, cyc.cycle, True))
+
+        df = pd.concat(df_list, axis='index')
+        df.to_csv(outfile, index=False)
 
 
-        # get hits during counting periods
-        cnt, err = np.array([c[2].get_counts('Li6') for c in cnt_cyc]).transpose()
-
-        # make output dataframe
-        df = pd.DataFrame({
-                        'run': run.run_number,
-                        'cycle': [c.cycle for c in cnt_cyc],
-                        'counts': cnt,
-                        'dcounts': err,
-                        'counts_bkgd': bk_mean,
-                        'dcounts_bkgd': bk_std,
-                        'irr_time_s': [c.cycle_param.period_durations_s[0] for c in cnt_cyc],
-                        'store_time_s': [c.cycle_param.period_durations_s[1] for c in cnt_cyc],
-                        'count_time_s': [c.cycle_param.period_durations_s[2] for c in cnt_cyc],
-                        'beam_uA': [c[0].beam_current_uA.mean() for c in cnt_cyc],
-                        'dbeam_uA': [c[0].beam_current_uA.std() for c in cnt_cyc],
-                        'beam_uA_bkgd': bk_currents.mean(),
-                        'dbeam_uA_bkgd': bk_currents.std(),
-                        'cycle_start': [c.cycle_start for c in cnt_cyc],
-                        })
-
-        # write
-        df.to_csv(filename_summary, mode='a', index=False, header=write_cols_names)
-        write_cols_names = False
 
 # load results
-df = pd.read_csv(filename_summary, comment='#')
+df = pd.read_csv(outfile, comment='#')
 
 # get rounded beam currents for grouping - find closest from list
 fn = lambda x: target_currents[np.argmin(np.abs(target_currents-x))]
-df['beam_rounded_uA'] = df['beam_uA'].apply(fn)
+df['current_rounded'] = df['current'].apply(fn)
 
-# get background-subtracted counts
-df['counts_corr'] = df.counts - df.counts_bkgd
-df['dcounts_corr'] = (df.dcounts**2 + df.dcounts_bkgd**2)**0.5
+# pair last 20 sec with corresponding run
+df.set_index(['run', 'cycle_num'], inplace=True)
+df_last = df.loc[df.is_last_20s]
+df = df.loc[~df.is_last_20s]
+df['last20_counts'] = df_last['counts']
+df['last20_bkgd'] = df_last['counts_bkgd']
 
-# drop bad cycles
-df.drop(df.index[df.counts_corr < 0], inplace=True)
+# get marked background cycles
+df['is_bkgd'] = df['counts_bkgd'] != 0
+
+# round irradiation times to nearest 10 sec
+df['irradiation_time'] = (df['irradiation_time']/10).round()*10
+
+# keep only storage time 10s (108/127)
+df = df.loc[df.storage_time == 10]
+
+# reset index
+df.reset_index(inplace=True)
+
+# correct backgrounds by rounded current (not every run has a background cycle)
+for cur, df_cur in df.groupby('current_rounded'):
+    for irr, df_irr in df_cur.groupby('irradiation_time'):
+        df_dat = df_irr.loc[~df_irr.is_bkgd]
+        df_bkg = df_irr.loc[df_irr.is_bkgd]
+
+        # subtract using nearest background
+        for i in df_dat.index:
+            closest = df_bkg.index[np.argmin(abs(df_bkg.index - i))]
+            df.loc[i, 'counts_bkgd'] = df_bkg.loc[closest, 'counts_bkgd']
+            df.loc[i, 'last20_bkgd'] = df_bkg.loc[closest, 'last20_bkgd']
+
+# ditch the background calculations
+df = df.loc[~df.is_bkgd]
+
+# setup analysis columns
+df['factor'] = df.last20_counts / df.last20_bkgd
+df['dfactor'] = df.factor * (1/df.last20_counts + 1/df.last20_bkgd)**0.5
+
+df['dbkgd_counts_corr'] = df.factor * df.counts_bkgd * (1/df.counts_bkgd + (df.dfactor/df.factor)**2)**0.5
+df['bkgd_counts_corr'] = df.factor * df.counts_bkgd
+
+df['counts_corr'] = df.counts - df.bkgd_counts_corr
+df['dcounts_corr'] = (df.counts + df.dbkgd_counts_corr**2)**0.5
 
 # fit function
 fitfn = lambda t, amp, tau : amp*(1-np.exp(-t/tau))
 
-# draw
-plt.figure()
 currents = []
 pars = []
 stds = []
 
-for current, g in df.groupby('beam_rounded_uA'):
+# draw and fit
+plt.figure()
+for cur, df_cur in df.groupby('current_rounded'):
 
     # draw
-    line = plt.errorbar(g.irr_time_s, g.counts_corr, g.dcounts_corr,
-                fmt='o', label=f'{int(current):d} $\\mu$A',
-                fillstyle='none')
+    x = df_cur.irradiation_time
+    y = df_cur.counts_corr
+    dy = df_cur.dcounts_corr
+    # y = df_cur.counts
+    # dy = df_cur.counts**0.5
+    line = plt.errorbar(x, y, dy, fmt='o', fillstyle='none', label=f'{int(cur):d} $\\mu$A')
 
     # fit
-    par, cov = curve_fit(fitfn, g.irr_time_s, g.counts_corr,
-                         sigma=g.dcounts_corr,
-                         absolute_sigma=True,
-                         p0=(g.counts_corr.max(), 1))
+    par, cov = curve_fit(fitfn, x, y, sigma=dy,
+                        absolute_sigma=True,
+                        p0=(max(y), 1))
     std = np.diag(cov)**0.5
 
-    fitx = np.linspace(g.irr_time_s.min(), g.irr_time_s.max(), 100)
+    fitx = np.linspace(min(x), max(x), 100)
     plt.plot(fitx, fitfn(fitx, *par), color=line[0].get_color())
 
-    currents.append(current)
+    currents.append(cur)
     pars.append(par)
     stds.append(std)
 
@@ -228,7 +251,7 @@ plt.ylabel('UCN Counts')
 plt.xlabel('Irradiation Time (s)')
 plt.legend(fontsize='x-small')
 plt.tight_layout()
-plt.savefig(f'{dirname}/{dirname}_fit.pdf')
+plt.savefig(f'{dirname}/TCN6A_040_fit.pdf')
 
 # draw fit parameters
 pars = np.transpose(pars)
@@ -240,14 +263,14 @@ plt.errorbar(currents, pars[0], stds[0], fmt='o', fillstyle='none')
 plt.ylabel('Amplitude')
 plt.xlabel(r'Current ($\mu$A)')
 plt.tight_layout()
-plt.savefig(f'{dirname}/{dirname}_amp.pdf')
+plt.savefig(f'{dirname}/TCN6A_040_amp.pdf')
 
 plt.figure()
 plt.errorbar(currents, pars[1], stds[1], fmt='o', fillstyle='none')
 plt.ylabel(r'Saturation Time $\tau$ (s)')
 plt.xlabel(r'Beam Current ($\mu$A)')
 plt.tight_layout()
-plt.savefig(f'{dirname}/{dirname}_tau.pdf')
+plt.savefig(f'{dirname}/TCN6A_040_tau.pdf')
 
 # RUN ==================================
-# extract_counts()
+# extract()
